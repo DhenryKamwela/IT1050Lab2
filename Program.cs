fbd8146fb7dfc26fd6824375975d2a9d72bbcaad
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-11-16 12:33:53 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Fayomi1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
